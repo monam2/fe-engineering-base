ed857e4ce9e9747f538429cf990d265a7f8370fe
monam2 <kangcw0107@gmail.com>
2025-11-23 20:14:41 +0900

    feat(react): If 컴포넌트

---
+++ packages/@fe-base/react/src/components/If/index.tsx
@@ -0,0 +1,18 @@
+interface IfProps {
+  condition: boolean | unknown;
+  trueRender?: React.ReactNode;
+  falseRender?: React.ReactNode;
+}
+
+/**
+ *
+ * @param condition - 조건
+ * @param trueRender - 조건이 true일 때 렌더링할 컴포넌트
+ * @param falseRender - 조건이 false일 때 렌더링할 컴포넌트
+ * @returns 렌더링할 컴포넌트(trueRender or falseRender)
+ */
+export function If({ condition, trueRender, falseRender }: IfProps) {
+  const conditionFlag = !!condition;
+
+  return conditionFlag ? trueRender : falseRender;
+}
